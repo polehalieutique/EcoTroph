7c2e592486aceddd2d800b57f07b7b66e2fe72ff
jerome.guitton@agrocampus-ouest.fr <jerome.guitton@agrocampus-ouest.fr>
2022-04-12 08:42:56 +0200

    Version de test demaragge

--- R/CTSA.catch.input.R
+++ R/CTSA.catch.input.R
@@ -1,3 +1,19 @@
+#' @title Catch input for CTSA
+#' @description   CTSA.catch.input is used to create inputs for the CTSA.forward function. It is a list of data.frames referring to catches per fleet formatted with TL classes in rows and trophic groups in columns.
+#' @param catch.group is a data.frame containing: a column group_name, column(s) referring to the catches of each fleet (named 'catch.1', 'catch.2'...), a column TL specifying the mean TL of each group, and optionally a column OI (omnivory index) used for smooth_type=3.
+#' @param smooth_type is a parameter of the create.smooth function. It defines the type of sigma calculation for the lognormal distribution. The value for this parameter is 1, 2 or 3. By default smooth_type=1, this defines a constant sigma. By choosing smooth_type=2, the user has the possibility to put a sigmaLN=smooth_param*ln(TL-0.05), with smooth_param=0.07 and shift=0.95 by default. Smooth_type=3 corresponds to the use of the calculated Omnivory Index (OI) divided by the associated mean TL as sigmaLN.
+#' @param sigmaLN_cst is a parameter of the create.smooth function. It defines the value of the constant sigma of the lognormal distribution for smooth_type=1. By default, sigmaLN_cst=0.12.
+#' @param pas is a parameter of the create.smooth function. It defines the splitting of the TL classes.
+#' @param shift is a parameter of the create.smooth function. It defines the beginning of the smooth function and allows the substraction of 0.05 in the sigma calculation accounting for the half interval range of the trophic class.
+#' @param smooth_param is a parameter of the create.smooth function. It defines the slope of the log-linearly increase of the TL variability with the mean trophic level of the group for smooth_type=2. SigmaLN(TL) is thus defined as sigmaLN(TL)=smooth_param*ln(TL-0.05).
+#' @seealso create.smooth, Transpose and CTSA.forward.
+#' @examples
+#' data(ecopath_guinee)
+#' catch.group=ecopath_guinee[,c("group_name","TL","catch.1","catch.2")]
+#' Y_test <- CTSA.catch.input(catch.group)
+#' Y_test
+#' @export
+#'
 CTSA.catch.input=function(catch.group,smooth_type=NULL,sigmaLN_cst=NULL,pas=NULL,shift=NULL,smooth_param=NULL){
   nm=colnames(catch.group)
   fleet=nm[substring(nm,1,6)=='catch.'];n.fleet=length(fleet)
--- R/E_MSY_0.1.R
+++ R/E_MSY_0.1.R
@@ -1,25 +1,19 @@
+#'  E_MSY_0.1 computes two indices of exploitation: Emsy or Fmsy (maximum sustainable yield), and E0.1 or F0.1 ("start" of full exploitation) per TL class.
+#' @param data is the list object returned by the create.ETmain function.
+#' @param Mul_eff is a parameter of the create.ETdiagnosis function. It is a vector of fishing effort multipliers that the user wants to test. Mul_eff must contain the value 1 (reference state). By default, the function simulates a range of fishing effort multipliers from 0 to 5 for each fleet.
+#' @param B.Input is a parameter of the create.ETdiagnosis function. It is a logical argument (default=F), if TRUE the "Biomass input control" equation is accounted for in EcoTroph equations.
+#' @param Beta is a parameter of the create.ETdiagnosis function. It is a coefficient expressing the extent of the biomass input control. Beta=0 refers to an ecosystem where all secondary production originates from grazing on primary producers, and Beta=1 to an ecosystem where detritus and/or recruitment contribute to a major part of the biomass input (default=0.2).
+#' @param TopD is a parameter of the create.ETdiagnosis function. It is a coefficient expressing the top-down control, i.e. the fraction of the natural mortality depending on predator abundance. It varies between 0 and 1. The user can specify a numeric value, which is applied to each trophic level, or a numeric vector (of the same length as TL classes), i.e. a value for each TL (default=0.4).
+#' @param FormD is a parameter of the create.ETdiagnosis function. It is a shape parameter varying between 0 and 1. It defines the functional relationship between prey and predators. The value 1 refers to a situation where predators abundance has a linear effect on the speed of the flow of their preys. The user can specify a numeric value, which is applied to each trophic level, or a numeric vector (of the same length as TL classes), i.e. a value for each TL (default=0.5).
+#' @param TLpred is a parameter of the create.ETdiagnosis function. It is the trophic level that the user considers to be the "predator" trophic classes start. The default value is 3.5.
+#' @param maxTL is a numeric string indicating the maximum TL for which indices are computed.
+#' @details For any TL class, if E0.1 and/or Emsy value(s) is(are) equal to the maximum effort multiplier tested (max(Mul_eff)), then E/F0.1 and/or E/Fmsy are set equal to NA.
 
-E0.1=function(TL,Y,Fish_mort){
-  y=Y[as.character(TL),];ff=Fish_mort[as.character(TL),]
-  if(length(y[!y==0])==0){f.01=c(NA,NA)}else{
-    #pente.origine=(y[2]-y[1])/(ff[2]-ff[1])
-    pente=(y[2:length(y)]-y[1:(length(y)-1)])/(ff[2:length(y)]-ff[1:(length(ff)-1)])
-    names(pente)=names(y[1:(length(y)-1)])
-    dif=abs(pente-0.1*pente[1])# pente[1]: pente à l'origine 
-    n=names(dif[dif==min(dif)]) ; if(length(n)>1){n=n[1]}
- f.01=c(ff[names(ff)==as.numeric(n)],n)}
- return(as.numeric(f.01))
-}
-
-Emsy=function(TL,Y,Fish_mort){
-  y=Y[as.character(TL),];ff=Fish_mort[as.character(TL),]
-  x=cbind(y,ff)
-  if(length(y[!y==0])==0){emsy=c(NA,NA)}else{
-  emsy=c(x[x[,1]==max(y)[1],'ff'][1],names(y[y==max(y)[1]])[1])}
-  return(as.numeric(emsy))
-}
-
-E_MSY_0.1=function(data,Mul_eff=NULL,B.Input=NULL,Beta=NULL,TopD=NULL,FormD=NULL,TLpred=NULL,maxTL=NULL){
+#' @examples
+#' data(ecopath_guinee)
+#' \donttest{E_MSY_0.1(create.ETmain(ecopath_guinee))}
+#' @export
+E_MSY_0.1<-function(data,Mul_eff=NULL,B.Input=NULL,Beta=NULL,TopD=NULL,FormD=NULL,TLpred=NULL,maxTL=NULL){
   #data=create.ETmain(ecopath_guinee)
   #Mul_eff=NULL;B.Input=NULL;Beta=NULL;TopD=NULL;FormD=NULL;TLpred=NULL;maxTL=NULL
   n.TL=nrow(data$ET_Main)
@@ -30,7 +24,7 @@ E_MSY_0.1=function(data,Mul_eff=NULL,B.Input=NULL,Beta=NULL,TopD=NULL,FormD=NULL
   if (is.null(FormD)){FormD <- rep(.5,n.TL)}else{if(length(FormD)==1){FormD=rep(FormD,n.TL)}}
   if (is.null(TLpred)){TLpred <- 3.5}
   if (is.null(maxTL)){maxTL=5.5}
-  
+
   diagn.list=create.ETdiagnosis(data=data,Mul_eff=Mul_eff,B.Input=B.Input,Beta=Beta,TopD=TopD,FormD=FormD,TLpred=TLpred,same.mE=T)
   fleet.of.interest=diagn.list[['fleet.of.interest']]
   if(!is.null(fleet.of.interest)){diagn.list=diagn.list[-length(diagn.list)]}
@@ -39,16 +33,16 @@ E_MSY_0.1=function(data,Mul_eff=NULL,B.Input=NULL,Beta=NULL,TopD=NULL,FormD=NULL
   for(i in 1:length(Mul_eff)){
    fm=diagn.list[[as.character(Mul_eff[i])]][['Fish_mort']]
    y=diagn.list[[as.character(Mul_eff[i])]][['Catches.tot']]
-  if(i==1){Fish_mort=fm;Y=y}else{Fish_mort=cbind(Fish_mort,fm);Y=cbind(Y,y)}  
+  if(i==1){Fish_mort=fm;Y=y}else{Fish_mort=cbind(Fish_mort,fm);Y=cbind(Y,y)}
   }
   colnames(Y)=Mul_eff;colnames(Fish_mort)=Mul_eff
-  
+
   E_MSY=sapply(row.names(Y),Emsy,Y,Fish_mort)
   E_0.1=sapply(row.names(Y),E0.1,Y,Fish_mort)
   MSY_0.1=cbind(t(E_MSY),t(E_0.1))
   colnames(MSY_0.1)=c('F_MSY','E_MSY','F_0.1','E_0.1')
   MSY_0.1=as.data.frame(MSY_0.1)
-  
+
   # Control, if E_MSY=max(Mul_eff)=> NA
   msy.na=rownames(MSY_0.1[MSY_0.1$E_MSY==max(Mul_eff) & !is.na(MSY_0.1$E_MSY),])
   if(!length(msy.na)==0){MSY_0.1[row.names(MSY_0.1)%in%msy.na,c(1,2)]=matrix(data=NA,ncol=2,nrow=length(msy.na))}
@@ -56,5 +50,5 @@ E_MSY_0.1=function(data,Mul_eff=NULL,B.Input=NULL,Beta=NULL,TopD=NULL,FormD=NULL
   m01.na=rownames(MSY_0.1[MSY_0.1$E_0.1==max(Mul_eff) & !is.na(MSY_0.1$E_0.1),])
   if(!length(m01.na)==0){MSY_0.1[row.names(MSY_0.1)%in%m01.na,c(1,2)]=matrix(data=NA,ncol=2,nrow=length(m01.na))}
 
-  MSY_0.1=MSY_0.1[as.numeric(row.names(MSY_0.1))<=maxTL,] 
+  MSY_0.1=MSY_0.1[as.numeric(row.names(MSY_0.1))<=maxTL,]
 return(MSY_0.1)}
